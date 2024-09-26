19e4600567dd0db7467f7a49c0bbd620083771f2
Nishwa Shaikh <nishwashaikh135@gmail.com>
2024-09-26 22:52:53 +0500

    Made some changes in Python file

--- PythonCodes.py
+++ PythonCodes.py
@@ -1,5 +1,5 @@
-#Swap two nums
-"""n=int(input("Enter any 2 digit number: "))
+#Swap two numbers
+n=int(input("Enter any 2 digit number: "))
 if n>9:
     n=str(n)
     print(n[::-1])
@@ -8,34 +8,34 @@ elif n>0 and n<=9:
     print(f"0{n}")
 else:
     n=str(n)
-    print(n[0]+n[:0:-1])"""
+    print(n[0]+n[:0:-1])
 
 #Checking if a number is prime or not
-"""n=int(input("Enter an integer: "))
+n=int(input("Enter an integer: "))
 count=0
 for i in range(2, n+1):
     if n%i==0:
         count+=1
 if count==1:
-    print("prime number")"""
+    print("prime number")
 
 #Calculating power
-'''base=int(input("Enter base: "))
+base=int(input("Enter base: "))
 power=int(input("Enter power: "))
 for i in range(1, power+1):
     Answer=base**i
-print(Answer)'''
+print(Answer)
 
 #Check if a number is palindrome
-'''n=int(input("Enter number: "))
+n=int(input("Enter number: "))
 n=str(n)
 if n[::]==n[::-1]:
     print("Palindrome")
 else:
-    print("Not a palindrome")'''
+    print("Not a palindrome")
 
 #Printing prime numbers between 2 intervals
-"""num1 = int(input("Enter any number: "))
+num1 = int(input("Enter any number: "))
 num2 = int(input("Enter another number: "))
 for i in range(num1+1, num2):
     count=0
@@ -44,17 +44,17 @@ for i in range(num1+1, num2):
             count+=1
             break
     if count==0 and i>1:
-        print(f"{i} is prime.")"""
+        print(f"{i} is prime.")
 
 #GCD
-"""Num1=int(input("Enter any number: "))
+Num1=int(input("Enter any number: "))
 Num2=int(input("Enter any number: "))
 while Num2!=0:
     Num1, Num2=Num2, Num1%Num2
-print(f"The GCD is {Num1}")"""
+print(f"The GCD is {Num1}")
 
 #LCM
-"""num1 = int(input("Enter the first number: "))
+num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 a, b = num1, num2
 while num2 != 0:
@@ -62,17 +62,16 @@ while num2 != 0:
 GCD = num1
 LCM = (a * b) // GCD
 print(f"The LCM of {a} and {b} is {LCM}")
-"""
+
 #Factorial
-"""n=int(input("Enter any number: "))
+n=int(input("Enter any number: "))
 count=1
 for i in range(1, n+1):
     count*=i
 print (count)
-"""
 
 #Fibonacci series
-"""a, b=0,1
+a, b=0,1
 n=int(input("Enter number: "))
 if n==1:
     print(a)
@@ -84,10 +83,9 @@ else:
         next=a+b
         print(next, end=" ") 
         a, b= b, next
-"""
 
 #Armstrong number
-'''Sum=0
+Sum=0
 n=int(input("Enter any number: "))
 n=str(n)
 for i in n:
@@ -96,10 +94,10 @@ for i in n:
 if Sum==int(n):
     print(f"{n} is an armstrong number")
 else:
-    print("Not an armstrong")'''
+    print("Not an armstrong")
 
 #Perfect number
-"""SUM=0
+SUM=0
 n=int(input("Enter number: "))
 for i in range(1, n):
     if n%i==0:
@@ -108,10 +106,10 @@ for i in range(1, n):
 if SUM==n:
     print(f"{n} is a perfect number")
 else:
-    print("Not a perfect number")"""
+    print("Not a perfect number")
 
 #Strong number:
-'''SUM=0
+SUM=0
 num=int(input("Enter number: "))
 STR=str(num)
 for i in STR:
@@ -123,16 +121,16 @@ for i in STR:
 if SUM==num:
     print("STRONG number!")
 else:
-    print("try another number")'''
+    print("try another number")
     
 #Swapping values by function
-'''def Swap(num1, num2):
+def Swap(num1, num2):
     print(f"Num1 before Swap is {num1}\nNum2 before swap is {num2}")
     num3=num2
     num2=num1
     num1=num3
     print(f"Num1 after swap is {num1}\nNum2 after swap is {num2}")
-Swap(2,4)'''
+Swap(2,4)
 
 #Factorial using reccursion
 def fact(n):
